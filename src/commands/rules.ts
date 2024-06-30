95649a4e2356837b47576e09f732a38cff6b925c
Krz <juniorkrz77@gmail.com>
2024-06-30 17:05:45 -0300

    Added command rules

---
+++ src/commands/rules.ts
@@ -0,0 +1,62 @@
+import { GroupMetadata } from '@whiskeysockets/baileys'
+import path from 'path'
+
+import { StickerBotCommand } from '../types/Command'
+import { WAMessageExtended } from '../types/Message'
+import { react, sendMessage } from '../utils/baileysHelper'
+import { checkCommand } from '../utils/commandValidator'
+import { capitalize, spintax } from '../utils/misc'
+
+// Gets the extension of this file, to dynamically import '.ts' if in development and '.js' if in production
+const extension = __filename.endsWith('.js') ? '.js' : '.ts'
+
+// Gets the file name without the .ts/.js extension
+const commandName = capitalize(path.basename(__filename, extension))
+
+// Command settings:
+export const command: StickerBotCommand = {
+  name: commandName,
+  aliases: ['regras', 'rules', 'rtfm'],
+  desc: 'Envia as regras do grupo.',
+  example: false,
+  needsPrefix: false,
+  inMaintenance: false,
+  runInPrivate: false,
+  runInGroups: true,
+  onlyInBotGroup: false,
+  onlyBotAdmin: false,
+  onlyAdmin: false,
+  botMustBeAdmin: false,
+  interval: 5,
+  limiter: {}, // do not touch this
+  run: async (
+    jid: string,
+    sender: string,
+    message: WAMessageExtended,
+    alias: string,
+    body: string,
+    group: GroupMetadata | undefined,
+    isBotAdmin: boolean,
+    isGroupAdmin: boolean,
+    amAdmin: boolean
+  ) => {
+    const check = await checkCommand(jid, message, alias, group, isBotAdmin, isGroupAdmin, amAdmin, command)
+    if (!check) {
+      return
+    }
+
+    let response
+
+    if (group) {
+      await react(message, '🤖')
+      response = group.desc || '¯\\_(ツ)_/¯'
+    } else {
+      response = '¯\\_(ツ)_/¯'
+    }
+
+    return await sendMessage(
+      { text: spintax(response) },
+      message
+    )
+  }
+}
