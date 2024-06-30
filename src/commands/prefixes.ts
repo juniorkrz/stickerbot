c7d0d60c0094484ee10a86cfccc826c59d85c315
Krz <juniorkrz77@gmail.com>
2024-06-30 16:52:07 -0300

    Added prefixes command

---
+++ src/commands/prefixes.ts
@@ -0,0 +1,63 @@
+import { GroupMetadata } from '@whiskeysockets/baileys'
+import path from 'path'
+
+import { bot } from '../config'
+import { StickerBotCommand } from '../types/Command'
+import { WAMessageExtended } from '../types/Message'
+import { sendMessage } from '../utils/baileysHelper'
+import { checkCommand } from '../utils/commandValidator'
+import { capitalize, spintax } from '../utils/misc'
+
+// Gets the extension of this file, to dynamically import '.ts' if in development and '.js' if in production
+const extension = __filename.endsWith('.js') ? '.js' : '.ts'
+
+// Gets the file name without the .ts/.js extension
+const commandName = capitalize(path.basename(__filename, extension))
+
+// Command settings:
+export const command: StickerBotCommand = {
+  name: commandName,
+  aliases: ['prefixos', 'prefixes'],
+  desc: 'Mostra os prefixos aceitos pelo bot.',
+  example: false,
+  needsPrefix: true,
+  inMaintenance: false,
+  runInPrivate: true,
+  runInGroups: true,
+  onlyInBotGroup: false,
+  onlyBotAdmin: false,
+  onlyAdmin: false,
+  botMustBeAdmin: false,
+  interval: 10,
+  limiter: {}, // do not touch this
+  run: async (
+    jid: string,
+    sender: string,
+    message: WAMessageExtended,
+    alias: string,
+    body: string,
+    group: GroupMetadata | undefined,
+    isBotAdmin: boolean,
+    isGroupAdmin: boolean,
+    amAdmin: boolean
+  ) => {
+    const check = await checkCommand(jid, message, alias, group, isBotAdmin, isGroupAdmin, amAdmin, command)
+    if (!check) {
+      return
+    }
+
+    let response = '🔣 *Prefixos*\n\n'
+    response += `O ${bot.name} aceita os seguintes prefixos:\n\n`
+
+    for (const prefix of bot.prefixes) {
+      response += `${prefix}\n`
+    }
+
+    response += '\n_Você pode enviar um comando com qualquer um deles no inicio da mensagem_'
+
+    return await sendMessage(
+      { text: spintax(response) },
+      message
+    )
+  }
+}
