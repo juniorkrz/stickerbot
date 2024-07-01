3044e7bb98878f8b58bd1ba3df04a233dc4dfc32
Krz <juniorkrz77@gmail.com>
2024-06-30 22:03:24 -0300

    Added command reply

---
+++ src/commands/reply.ts
@@ -0,0 +1,87 @@
+import { GroupMetadata } from '@whiskeysockets/baileys'
+import path from 'path'
+
+import { getStore } from '../bot'
+import { bot } from '../config'
+import { StickerBotCommand } from '../types/Command'
+import { WAMessageExtended } from '../types/Message'
+import { getBody, getQuotedMessage, react, sendMessage } from '../utils/baileysHelper'
+import { checkCommand } from '../utils/commandValidator'
+import { capitalize } from '../utils/misc'
+
+// Gets the extension of this file, to dynamically import '.ts' if in development and '.js' if in production
+const extension = __filename.endsWith('.js') ? '.js' : '.ts'
+
+// Gets the file name without the .ts/.js extension
+const commandName = capitalize(path.basename(__filename, extension))
+
+// Command settings:
+export const command: StickerBotCommand = {
+  name: commandName,
+  aliases: ['reply'],
+  desc: 'Envia resposta ao feedback.',
+  example: false,
+  needsPrefix: true,
+  inMaintenance: false,
+  runInPrivate: true,
+  runInGroups: true,
+  onlyInBotGroup: false,
+  onlyBotAdmin: false,
+  onlyAdmin: true,
+  botMustBeAdmin: false,
+  interval: 0,
+  limiter: {}, // do not touch this
+  run: async (
+    jid: string,
+    sender: string,
+    message: WAMessageExtended,
+    alias: string,
+    body: string,
+    group: GroupMetadata | undefined,
+    isBotAdmin: boolean,
+    isGroupAdmin: boolean,
+    amAdmin: boolean
+  ) => {
+    const check = await checkCommand(jid, message, alias, group, isBotAdmin, isGroupAdmin, amAdmin, command)
+    if (!check) {
+      return
+    }
+
+    // If there is no group of logs, do nothing
+    if (!bot.logsGroup) return
+
+    // If it's not the log group, don't do anything
+    if (jid != bot.logsGroup) return
+
+    // Get the quoted message
+    const quotedMsg = getQuotedMessage(message)
+    if (!quotedMsg) return await react(message, '❌')
+
+    try {
+      // Extract feedback JID and stanza ID from the quoted message
+      const quotedBody = getBody(quotedMsg)
+      const splittedQB = quotedBody.split(' ')
+      const feedbackJid = splittedQB[0].split('_')[0]
+      const stanzaId = splittedQB[0].split('_')[1].split('\n')[0]
+
+      // Retrieve the feedback message from the store
+      const store = getStore()
+      const chatMessages = store.messages[feedbackJid]
+      const feedbackMsg = chatMessages?.get(stanzaId)
+
+      if (!feedbackMsg) return await react(message, '❌')
+
+      // Prepare the response message
+      let response = body.slice(command.needsPrefix ? 1 : 0).replace(alias, '').trim()
+      response += `\n\n_Mensagem enviada pelo administrador: ${message.pushName}_`
+
+      // Send the response message
+      await sendMessage({ text: response }, feedbackMsg)
+
+      // React to the message with a success emoji
+      return await react(message, '✅')
+    } catch {
+      return await react(message, '❌')
+    }
+  }
+}
