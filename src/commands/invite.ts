c335ae7ed522171a2c84912ecac626b1178d5677
Krz <juniorkrz77@gmail.com>
2024-07-02 13:46:53 -0300

    Added invite command

---
+++ src/commands/invite.ts
@@ -0,0 +1,58 @@
+import { GroupMetadata } from '@whiskeysockets/baileys'
+import path from 'path'
+
+import { getClient } from '../bot'
+import { StickerBotCommand } from '../types/Command'
+import { WAMessageExtended } from '../types/Message'
+import { sendMessage } from '../utils/baileysHelper'
+import { checkCommand } from '../utils/commandValidator'
+import { capitalize } from '../utils/misc'
+
+// Gets the extension of this file, to dynamically import '.ts' if in development and '.js' if in production
+const extension = __filename.endsWith('.js') ? '.js' : '.ts'
+
+// Gets the file name without the .ts/.js extension
+const commandName = capitalize(path.basename(__filename, extension))
+
+// Command settings:
+export const command: StickerBotCommand = {
+  name: commandName,
+  aliases: ['convite', 'invite'],
+  desc: 'Envia o link de convite do grupo.',
+  example: false,
+  needsPrefix: true,
+  inMaintenance: false,
+  runInPrivate: false,
+  runInGroups: true,
+  onlyInBotGroup: false,
+  onlyBotAdmin: false,
+  onlyAdmin: true,
+  botMustBeAdmin: true,
+  interval: 30,
+  limiter: {},// do not touch this
+  run: async (
+    jid: string,
+    sender: string,
+    message: WAMessageExtended,
+    alias: string,
+    body: string,
+    group: GroupMetadata | undefined,
+    isBotAdmin: boolean,
+    isGroupAdmin: boolean,
+    amAdmin: boolean
+  ) => {
+    const check = await checkCommand(jid, message, alias, group, isBotAdmin, isGroupAdmin, amAdmin, command)
+    if (!check) return
+
+    // get the client
+    const client = getClient()
+
+    // get the invite code
+    const code = await client.groupInviteCode(jid)
+
+    return await sendMessage(
+      { text: `https://chat.whatsapp.com/${code}` },
+      message
+    )
+  }
+}
