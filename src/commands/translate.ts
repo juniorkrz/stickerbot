5ac6630c5dd9ad8cac4b8f3038758b8f8523829c
Krz <juniorkrz77@gmail.com>
2024-07-31 22:59:47 -0300

    Added translate command

---
+++ src/commands/translate.ts
@@ -0,0 +1,91 @@
+import { translate } from '@vitalets/google-translate-api'
+import { GroupMetadata } from '@whiskeysockets/baileys'
+import path from 'path'
+
+import { getLogger } from '../handlers/logger'
+import { StickerBotCommand } from '../types/Command'
+import { WAMessageExtended } from '../types/Message'
+import { getBody, getCaption, getQuotedMessage, sendLogToAdmins, sendMessage } from '../utils/baileysHelper'
+import { checkCommand } from '../utils/commandValidator'
+import { capitalize, spintax } from '../utils/misc'
+
+// Gets the logger
+const logger = getLogger()
+
+// Gets the extension of this file, to dynamically import '.ts' if in development and '.js' if in production
+const extension = __filename.endsWith('.js') ? '.js' : '.ts'
+
+// Gets the file name without the .ts/.js extension
+const commandName = capitalize(path.basename(__filename, extension))
+
+// Command settings:
+export const command: StickerBotCommand = {
+  name: commandName,
+  aliases: ['traduzir', 'translate'],
+  desc: 'Traduz o texto enviado ou a mensagem citada para pt-Br.',
+  example: 'Hello World!',
+  needsPrefix: true,
+  inMaintenance: false,
+  runInPrivate: true,
+  runInGroups: true,
+  onlyInBotGroup: false,
+  onlyBotAdmin: false,
+  onlyAdmin: false,
+  onlyVip: false,
+  botMustBeAdmin: false,
+  interval: 5,
+  limiter: {}, // do not touch this
+  run: async (
+    jid: string,
+    sender: string,
+    message: WAMessageExtended,
+    alias: string,
+    body: string,
+    group: GroupMetadata | undefined,
+    isBotAdmin: boolean,
+    isVip: boolean,
+    isGroupAdmin: boolean,
+    amAdmin: boolean
+  ) => {
+    const check = await checkCommand(jid, message, alias, group, isBotAdmin, isVip, isGroupAdmin, amAdmin, command)
+    if (!check) return
+
+    const maxChars = 10000
+    const quotedMsg = getQuotedMessage(message)
+    const text = quotedMsg
+      ? getBody(quotedMsg) || getCaption(quotedMsg)
+      : body.slice(command.needsPrefix ? 1 : 0).replace(new RegExp(alias, 'i'), '').trim()
+
+    if (!text) {
+      return await sendMessage(
+        {
+          text: spintax(`⚠ {Ei|Ops|Opa|Desculpe|Foi mal}, {para|pra} {utilizar|usar} o comando *${alias}* ` +
+            '{você|vc|tu} {precisa|deve} {escrever|digitar} {um texto|algo} {após |depois d}o comando. {🧐|🫠|🥲|🙃|📝}')
+        },
+        message
+      )
+    } else if (text.length > maxChars) {
+      return await sendMessage(
+        { text: spintax(`⚠ O texto deve ter no máximo *${maxChars}* caracteres!`) },
+        message
+      )
+    }
+
+    try {
+      const result = await translate(text, { to: 'pt' })
+      return await sendMessage(
+        { text: `*Tradutor*: ${result.text}` },
+        message
+      )
+    } catch (error) {
+      logger.warn('API: translate is down!')
+      await sendLogToAdmins('*[API]:* translate está offline!')
+      const reply = '⚠ Desculpe, este serviço está indisponível no momento. Por favor, tente novamente mais tarde.'
+      await sendMessage(
+        { text: reply },
+        message
+      )
+    }
+
+  }
+}
